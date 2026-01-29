ce06ee47aa9a8bc6eccde119da438c8e3a1ae490
pokoprogrammer12 <153587842+pokoprogrammer12@users.noreply.github.com~>
2026-01-29 22:35:19 +0100

    Adding new color theme and keymaps

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
+--required packs
 require 'core.options'
 require 'core.keymaps'
 
+--lazy and neotree setup  
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -41,8 +41,8 @@ vim.keymap.set('n', '<C-Right>', ':wincmd l<CR>', opts)
 -- Resize splits with Alt + arrows
 vim.keymap.set('n', '<A-Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<A-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<A-Right>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<A-Left>', ':vertical resize +2<CR>', opts)
 
 -- Move cursor in file with arrow keys
 vim.keymap.set('n', '<Up>', 'k', opts)
